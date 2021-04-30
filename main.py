be0a27653055360b4f83b4561f6e02f778b68477
MCruces-fz <mcsquared.fz@gmail.com>
2021-04-30 16:54:12 +0200

    Update license

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
 """
+             A P A C H E   L I C E N S E
+                    ------------ 
+              Version 2.0, January 2004
+
+       Copyright 2021 Miguel Cruces Fernández
+
+  Licensed under the Apache License, Version 2.0 (the 
+"License"); you may not use this file except in compliance 
+with the License. You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, 
+software distributed under the License is distributed on an 
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+either express or implied. See the License for the specific 
+language governing permissions and limitations under the 
+License.
+
+           miguel.cruces.fernandez@usc.es
+               mcsquared.fz@gmail.com
+
 # ================ B O T   D A T A  ================ #
 name: "TRAGALDABAS-alarms"
 username: "tragaldabas_bot"
@@ -67,5 +89,3 @@ def main(print_status: bool = False):
 
 if __name__ == '__main__':
     main(print_status=True)
-
-# TODO: print bash return: "ls -la | tail -5" (function)
